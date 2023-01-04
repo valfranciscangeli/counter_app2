0e225372c7f53504861af7c4c603370b7e50160f
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-04 17:48:40 -0300

    fin clase 52

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,16 +6,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var fontSize = const TextStyle(fontSize: 20);
+
     return Scaffold(
       // scaffold le da como un tema pa que se vea astetik
 
-      backgroundColor: const Color.fromARGB(255, 229, 162, 196),
+      backgroundColor: const Color.fromARGB(255, 230, 124, 177),
       appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
         title: const Text('HomeScreen'),
         elevation: 1.0, // si le damos mas deja una sombra
       ),
-      body: const Center(
-        child: Text('Hola desde HomeScreen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // relativo al widget de mayor tamanho
+          children: [
+            // <widget> significa que todo lo que ponga dentro de las llaves debe ser un widget
+            Text(
+              'clicks counter',
+              style: fontSize,
+            ),
+            Text(
+              '0',
+              style: fontSize,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.plus_one),
+        onPressed: () {
+          print('Hola \n');
+        },
+        backgroundColor: Colors.deepPurpleAccent,
       ),
     );
   }
